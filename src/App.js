1a6b06a33abbb69aed4c7ffc4d6d13ffc7a123e4
freddy vidigal <freddy181208@hotmail.com>
2023-02-22 15:15:27 -0300

    meu app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Membro from "./component/Membro";
+class App extends Component{
+
+  render(){
+    return(
+      <div>
+        <Membro nome='visitante'/>
+      </div>
+    );
+  }
+}
+
+export default App;
